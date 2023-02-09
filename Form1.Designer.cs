a833b4f0b44c3815dd7bd0920e6bce11ae47302b
admin <admin@DESKTOP-LN985A8>
2023-02-09 09:16:35 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace minipauta_Romulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvPauta = new System.Windows.Forms.DataGridView();
+            this.Número = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Freguesia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AnoDeNascimento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sexo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Portugues = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.INGLES = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Filosofia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Matematica = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fisica = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quimica = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Geografia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Historia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.EducacaoFisica = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Moral = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Média = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Negativas = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblQuantasRaparigas = new System.Windows.Forms.Label();
+            this.lblRapazesVivemEmMaximinios = new System.Windows.Forms.Label();
+            this.lblMaisRapazesOuRaparigas = new System.Windows.Forms.Label();
+            this.lblOmaisVelho = new System.Windows.Forms.Label();
+            this.lblReprovadosTemMediaSuperiorDoze = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtRespostasDe1a5 = new System.Windows.Forms.TextBox();
+            this.txtRespostasDe6a10 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPauta)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPauta
+            // 
+            this.dgvPauta.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvPauta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPauta.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Número,
+            this.Nome,
+            this.Freguesia,
+            this.AnoDeNascimento,
+            this.Sexo,
+            this.Portugues,
+            this.INGLES,
+            this.Filosofia,
+            this.Matematica,
+            this.Fisica,
+            this.Quimica,
+            this.Geografia,
+            this.Historia,
+            this.EducacaoFisica,
+            this.Moral,
+            this.Média,
+            this.Negativas});
+            this.dgvPauta.Location = new System.Drawing.Point(0, 0);
+            this.dgvPauta.Name = "dgvPauta";
+            this.dgvPauta.Size = new System.Drawing.Size(1105, 496);
+            this.dgvPauta.TabIndex = 0;
+            // 
+            // Número
+            // 
+            this.Número.HeaderText = "N.";
+            this.Número.Name = "Número";
+            this.Número.Width = 43;
+            // 
+            // Nome
+            // 
+            this.Nome.HeaderText = "Nome";
+            this.Nome.Name = "Nome";
+            this.Nome.Width = 60;
+            // 
+            // Freguesia
+            // 
+            this.Freguesia.HeaderText = "FREGUESIA";
+            this.Freguesia.Name = "Freguesia";
+            this.Freguesia.Width = 93;
+            // 
+            // AnoDeNascimento
+            // 
+            this.AnoDeNascimento.HeaderText = "Ano";
+            this.AnoDeNascimento.Name = "AnoDeNascimento";
+            this.AnoDeNascimento.Width = 51;
+            // 
+            // Sexo
+            // 
+            this.Sexo.HeaderText = "Sexo";
+            this.Sexo.Name = "Sexo";
+            this.Sexo.Width = 56;
+            // 
+            // Portugues
+            // 
+            this.Portugues.HeaderText = "POR";
+            this.Portugues.Name = "Portugues";
+            this.Portugues.Width = 55;
+            // 
+            // INGLES
+            // 
+            this.INGLES.HeaderText = "ING";
+            this.INGLES.Name = "INGLES";
+            this.INGLES.Width = 51;
+            // 
+            // Filosofia
+            // 
+            this.Filosofia.HeaderText = "FIL";
+            this.Filosofia.Name = "Filosofia";
+            this.Filosofia.Width = 47;
+            // 
+            // Matematica
+            // 
+            this.Matematica.HeaderText = "MAT";
+            this.Matematica.Name = "Matematica";
+            this.Matematica.Width = 55;
+            // 
+            // Fisica
+            // 
+            this.Fisica.HeaderText = "FIS";
+            this.Fisica.Name = "Fisica";
+            this.Fisica.Width = 48;
+            // 
+            // Quimica
+            // 
+            this.Quimica.HeaderText = "QUI";
+            this.Quimica.Name = "Quimica";
+            this.Quimica.Width = 51;
+            // 
+            // Geografia
+            // 
+            this.Geografia.HeaderText = "GEO";
+            this.Geografia.Name = "Geografia";
+            this.Geografia.Width = 55;
+            // 
+            // Historia
+            // 
+            this.Historia.HeaderText = "HIS";
+            this.Historia.Name = "Historia";
+            this.Historia.Width = 50;
+            // 
+            // EducacaoFisica
+            // 
+            this.EducacaoFisica.HeaderText = "EF";
+            this.EducacaoFisica.Name = "EducacaoFisica";
+            this.EducacaoFisica.Width = 45;
+            // 
+            // Moral
+            // 
+            this.Moral.HeaderText = "MOR";
+            this.Moral.Name = "Moral";
+            this.Moral.Width = 57;
+            // 
+            // Média
+            // 
+            this.Média.HeaderText = "Média";
+            this.Média.Name = "Média";
+            this.Média.Width = 61;
+            // 
+            // Negativas
+            // 
+            this.Negativas.HeaderText = "NEGATIV.";
+            this.Negativas.Name = "Negativas";
+            this.Negativas.Width = 82;
+            // 
+            // lblQuantasRaparigas
+            // 
+            this.lblQuantasRaparigas.AutoSize = true;
+            this.lblQuantasRaparigas.Location = new System.Drawing.Point(12, 508);
+            this.lblQuantasRaparigas.Name = "lblQuantasRaparigas";
+            this.lblQuantasRaparigas.Size = new System.Drawing.Size(121, 13);
+            this.lblQuantasRaparigas.TabIndex = 1;
+            this.lblQuantasRaparigas.Text = "1 quantas raparigas há?";
+            this.lblQuantasRaparigas.Click += new System.EventHandler(this.lblQuantasRaparigas_Click);
+            // 
+            // lblRapazesVivemEmMaximinios
+            // 
+            this.lblRapazesVivemEmMaximinios.AutoSize = true;
+            this.lblRapazesVivemEmMaximinios.Location = new System.Drawing.Point(12, 534);
+            this.lblRapazesVivemEmMaximinios.Name = "lblRapazesVivemEmMaximinios";
+            this.lblRapazesVivemEmMaximinios.Size = new System.Drawing.Size(187, 13);
+            this.lblRapazesVivemEmMaximinios.TabIndex = 2;
+            this.lblRapazesVivemEmMaximinios.Text = "2 quantos rapazes são de Maximinos?";
+            this.lblRapazesVivemEmMaximinios.Click += new System.EventHandler(this.lblRapazesVivemEmMaximinios_Click);
+            // 
+            // lblMaisRapazesOuRaparigas
+            // 
+            this.lblMaisRapazesOuRaparigas.AutoSize = true;
+            this.lblMaisRapazesOuRaparigas.Location = new System.Drawing.Point(12, 556);
+            this.lblMaisRapazesOuRaparigas.Name = "lblMaisRapazesOuRaparigas";
+            this.lblMaisRapazesOuRaparigas.Size = new System.Drawing.Size(159, 13);
+            this.lblMaisRapazesOuRaparigas.TabIndex = 3;
+            this.lblMaisRapazesOuRaparigas.Text = "3 há mais rapazes ou raparigas?";
+            this.lblMaisRapazesOuRaparigas.Click += new System.EventHandler(this.lblMaisRapazesOuRaparigas_Click);
+            // 
+            // lblOmaisVelho
+            // 
+            this.lblOmaisVelho.AutoSize = true;
+            this.lblOmaisVelho.Location = new System.Drawing.Point(12, 578);
+            this.lblOmaisVelho.Name = "lblOmaisVelho";
+            this.lblOmaisVelho.Size = new System.Drawing.Size(119, 13);
+            this.lblOmaisVelho.TabIndex = 4;
+            this.lblOmaisVelho.Text = "4 quem é o mais velho?";
+            this.lblOmaisVelho.Click += new System.EventHandler(this.lblOmaisVelho_Click);
+            // 
+            // lblReprovadosTemMediaSuperiorDoze
+            // 
+            this.lblReprovadosTemMediaSuperiorDoze.AutoSize = true;
+            this.lblReprovadosTemMediaSuperiorDoze.Location = new System.Drawing.Point(12, 606);
+            this.lblReprovadosTemMediaSuperiorDoze.Name = "lblReprovadosTemMediaSuperiorDoze";
+            this.lblReprovadosTemMediaSuperiorDoze.Size = new System.Drawing.Size(231, 13);
+            this.lblReprovadosTemMediaSuperiorDoze.TabIndex = 5;
+            this.lblReprovadosTemMediaSuperiorDoze.Text = "5 quantos reprovados têm média superior a 12?";
+            this.lblReprovadosTemMediaSuperiorDoze.Click += new System.EventHandler(this.lblReprovadosTemMediaSuperiorDoze_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(506, 508);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(274, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "6 listar por ordem decrescente, os nomes dos 3 melhores";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(506, 534);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(169, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "7 listar os apelidos dos reprovados";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(506, 556);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(250, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "8 ordenar a grid pela média, por ordem decrescente";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(506, 578);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(412, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "9 criar uma List com os nomes de todos os formandos; copiar da List para uma List" +
+    "Box";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(506, 606);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(411, 13);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "10 criar uma List com os nomes e freguesias de todos os formandos; copiar p/ List" +
+    "Box";
+            // 
+            // txtRespostasDe1a5
+            // 
+            this.txtRespostasDe1a5.Location = new System.Drawing.Point(232, 553);
+            this.txtRespostasDe1a5.Name = "txtRespostasDe1a5";
+            this.txtRespostasDe1a5.Size = new System.Drawing.Size(183, 20);
+            this.txtRespostasDe1a5.TabIndex = 11;
+            // 
+            // txtRespostasDe6a10
+            // 
+            this.txtRespostasDe6a10.Location = new System.Drawing.Point(964, 549);
+            this.txtRespostasDe6a10.Name = "txtRespostasDe6a10";
+            this.txtRespostasDe6a10.Size = new System.Drawing.Size(100, 20);
+            this.txtRespostasDe6a10.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1136, 628);
+            this.Controls.Add(this.txtRespostasDe6a10);
+            this.Controls.Add(this.txtRespostasDe1a5);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblReprovadosTemMediaSuperiorDoze);
+            this.Controls.Add(this.lblOmaisVelho);
+            this.Controls.Add(this.lblMaisRapazesOuRaparigas);
+            this.Controls.Add(this.lblRapazesVivemEmMaximinios);
+            this.Controls.Add(this.lblQuantasRaparigas);
+            this.Controls.Add(this.dgvPauta);
+            this.Name = "Form1";
+            this.Text = "Mini Projeto Pauta Romulo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPauta)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPauta;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Número;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Freguesia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AnoDeNascimento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sexo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Portugues;
+        private System.Windows.Forms.DataGridViewTextBoxColumn INGLES;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Filosofia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Matematica;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fisica;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quimica;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Geografia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Historia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EducacaoFisica;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Moral;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Média;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Negativas;
+        private System.Windows.Forms.Label lblQuantasRaparigas;
+        private System.Windows.Forms.Label lblRapazesVivemEmMaximinios;
+        private System.Windows.Forms.Label lblMaisRapazesOuRaparigas;
+        private System.Windows.Forms.Label lblOmaisVelho;
+        private System.Windows.Forms.Label lblReprovadosTemMediaSuperiorDoze;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtRespostasDe1a5;
+        private System.Windows.Forms.TextBox txtRespostasDe6a10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace minipauta_Romulo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void ColonizarGrid()
+        {
+            //dgvPauta.Columns.Add("Nome",) ;
+
+            dgvPauta.Rows.Add("1", "Ana Rita Cunha", "Gualtar", "1950", 'F', "10", "11", "12", "10", "11", "12", "10", "11", "12", "11", "--", "--");
+            dgvPauta.Rows.Add("2", "Bela Costa Silva", "Maximinos", "1999", "F", "15", "11", "12", "10", "11", "12", "10", "11", "12", "8", "--", "--");
+            dgvPauta.Rows.Add("3", "Carlos Alberto Costa", "Aveleda", "2000", "M", "10", "11", "12", "10", "11", "12", "10", "19", "12", "12", "--", "--");
+            dgvPauta.Rows.Add("4", "Carlos Daniel Ferreira", "Gualtar", "2000", "M", "10", "11", "12", "10", "11", "12", "10", "11", "12", "8", "--", "--");
+            dgvPauta.Rows.Add("5", "Daniel Bastos Gomes", "Maximinos", "2001", "M", "10", "11", "8", "10", "11", "12", "10", "11", "12", "18", "--", "--");
+            dgvPauta.Rows.Add("6", "Daniel Silva Ferraz", "Ferreiros", "1997", "M", "10", "11", "12", "10", "11", "12", "7", "11", "12", "8", "--", "--");
+            dgvPauta.Rows.Add("7", "Elvira Gomes Pendes", "Maximinos", "2002", "F", "10", "11", "12", "10", "11", "12", "10", "11", "12", "13", "--", "--");
+            dgvPauta.Rows.Add("8", "Fernanda Maria Silva", "Aveleda", "2001", "F", "10", "11", "12", "10", "11", "12", "10", "11", "12", "8", "--", "--");
+            dgvPauta.Rows.Add("9", "Fernando Gomes Barros", "Gualtar", "1997", "M", "7", "11", "12", "10", "9", "12", "10", "11", "12", "16", "--", "--");
+            dgvPauta.Rows.Add("10", "Guilherme Alexandre Barros", "Aveleda", "1997", "M", "10", "11", "19", "10", "11", "12", "10", "11", "12", "8", "--", "--");
+            dgvPauta.Rows.Add("11", "Hilda Fonseca Silva", "Maximinos", "1997", "F", "10", "11", "12", "10", "11", "12", "10", "11", "12", "13", "--", "--");
+            dgvPauta.Rows.Add("12", "José Manuel Carvalho", "Sé", "2001", "M", "10", "11", "16", "10", "11", "12", "10", "11", "12", "13", "--", "--");
+            dgvPauta.Rows.Add("13", "José Alberto Gomes", "Maximinos", "2002", "M", "10", "11", "12", "10", "11", "12", "10", "11", "12", "8", "--", "--");
+            dgvPauta.Rows.Add("14", "Maria Silvéria Bastos", "Lovios", "2001", "F", "10", "11", "12", "16", "11", "12", "10", "11", "12", "8", "--", "--");
+            dgvPauta.Rows.Add("15", "Anabela Bastos Torres", "Ferreiros", "1997", "F", "10", "11", "12", "10", "11", "12", "10", "11", "12", "14", "--", "--");
+            dgvPauta.Rows.Add("16", "Rui Vasco Santos", "Maximinos", "2001", "M", "10", "11", "12", "10", "15", "12", "10", "11", "12", "5", "--", "--");
+            dgvPauta.Rows.Add("17", "Otávio Ferreira", "Gualtar", "2002", "M", "10", "11", "12", "10", "11", "12", "10", "11", "12", "8", "--", "--");
+            dgvPauta.Rows.Add("18", "Silvério Silva Teixeira", "Ferreiros", "1997", "M", "10", "11", "12", "10", "11", "12", "10", "11", "12", "8", "--", "--");
+            dgvPauta.Rows.Add("19", "Teodoro Armando Matos", "Maximinos", "2004", "M", "10", "11", "12", "10", "11", "12", "10", "11", "12", "8", "--", "--");
+            dgvPauta.Rows.Add("20", "Zacarias Alexandre Sampaio", "Sequeira", "1995", "M", "10", "11", "12", "10", "11", "12", "10", "11", "12", "8", "--", "--");
+
+
+        }
+
+        public void ContarNegativas()
+        {
+            double linhastotais = dgvPauta.Rows.Count - 1;
+            double Negativas = 0;
+
+            for (int i = 0; i < linhastotais; i++)
+            {
+                for (int j = 5; j < 15; j++)
+                {
+                    if (Convert.ToDouble(dgvPauta.Rows[i].Cells[j].Value) < 10)
+                    {
+                        Negativas++;
+                    }
+
+                }
+                dgvPauta.Rows[i].Cells[16].Value = Negativas;
+                Negativas = 0;
+            }
+
+        }
+
+
+        public void CalculaMedia()
+
+        { double media = 0;
+            double soma = 0;
+            double linhastotais = dgvPauta.Rows.Count - 1;
+
+            for (int i = 0; i < linhastotais; i++)
+            {
+                for (int j = 5; j < 15; j++)
+                {
+                    soma = soma + (Convert.ToDouble(dgvPauta.Rows[i].Cells[j].Value));
+                }
+
+                media = soma / 10;
+                dgvPauta.Rows[i].Cells[15].Value = Convert.ToString(media);
+                soma = 0;
+            }
+        }
+
+        //private bool Aprovado(double[] notas)
+        //{
+        //    int contagemNotasNegativas = 0;
+
+        //    foreach (double nota in notas)
+        //    {
+        //        if (nota < 0)
+        //        {
+        //            contagemNotasNegativas++;
+        //        }
+        //    }
+
+        //    if (contagemNotasNegativas >= (notas.Length / 2))
+        //    {
+        //        return false;
+        //    }
+        //    else
+        //    {
+        //        return true;
+        //    }
+        //}
+
+        public void contargenerosF()
+
+        {
+            double quantosrapazes = 0;
+            double quantasraparigas = 0;
+            double quantosoutros = 0;
+
+            double linhastotais = dgvPauta.Rows.Count - 1;
+
+            for (int i = 0; i < linhastotais; i++)
+            {
+                if (Convert.ToString(dgvPauta.Rows[i].Cells[4].Value) == "F")
+                {
+                    quantasraparigas++;
+                }
+                if (Convert.ToString(dgvPauta.Rows[i].Cells[4].Value) == "M")
+                {
+                    quantosrapazes++;
+                }
+                else
+                {
+                    quantosoutros++;
+                }
+
+            }
+            txtRespostasDe1a5.Text = "";
+            txtRespostasDe1a5.Text = Convert.ToString(quantasraparigas);
+
+
+        }
+
+        public void rapazesVivemEmMaximinios()
+        {
+            double rapazesDeMaximinios = 0;
+            double vivemEmOutros = 0;
+            double linhastotais = dgvPauta.Rows.Count - 1;
+
+            for (int i = 0; i < linhastotais; i++)
+            {
+                if (Convert.ToString(dgvPauta.Rows[i].Cells[4].Value) != "F")
+                {
+                    if (Convert.ToString(dgvPauta.Rows[i].Cells[2].Value) == "Maximinos")
+                    {
+                        rapazesDeMaximinios++;
+                    }
+                    
+                }
+                else
+                    {
+                        vivemEmOutros++;
+                    }
+            }
+            txtRespostasDe1a5.Text = "";
+            txtRespostasDe1a5.Text = Convert.ToString(rapazesDeMaximinios);
+
+        }
+
+        public void maisRaparigasOuRapazes()
+
+        {
+            double quantosrapazes = 0;
+            double quantasraparigas = 0;
+            double quantosoutros = 0;
+
+            double linhastotais = dgvPauta.Rows.Count - 1;
+
+            for (int i = 0; i < linhastotais; i++)
+            {
+                if (Convert.ToString(dgvPauta.Rows[i].Cells[4].Value) == "F")
+                {
+                    quantasraparigas++;
+                }
+                if (Convert.ToString(dgvPauta.Rows[i].Cells[4].Value) == "M")
+                {
+                    quantosrapazes++;
+                }
+                else
+                {
+                    quantosoutros++;
+                }
+
+            }
+            if (quantasraparigas>quantosrapazes)
+            {
+                txtRespostasDe1a5.Text = "";
+                txtRespostasDe1a5.Text = "Raparigas";
+            }
+            else
+            {
+                txtRespostasDe1a5.Text = "";
+                txtRespostasDe1a5.Text = ("Rapazes");
+            }
+            
+        }
+
+        public void oMaisVelho()
+
+        {
+            string oNomeMaisVelho = Convert.ToString(dgvPauta.Rows[1].Cells[1].Value);
+            double oAnoDoMaisVelho = Convert.ToInt64(dgvPauta.Rows[1].Cells[3].Value);
+
+            double linhastotais = dgvPauta.Rows.Count - 1;
+
+            for (int i = 0; i < linhastotais; i++)
+            {
+                if (oAnoDoMaisVelho > Convert.ToInt64(dgvPauta.Rows[i].Cells[3].Value))
+                {
+                    oAnoDoMaisVelho = Convert.ToInt64(dgvPauta.Rows[i].Cells[3].Value);
+                    oNomeMaisVelho = Convert.ToString(dgvPauta.Rows[i].Cells[1].Value);
+                }
+                
+                else
+                {
+                    ;
+                }
+
+            }
+           
+            txtRespostasDe1a5.Text = "";
+            txtRespostasDe1a5.Text = Convert.ToString(oNomeMaisVelho);
+           
+
+        }
+
+        public void reprovadosMediaMaiorQueDoze()
+
+        {
+            double reprovadosMediaMaior = 0;
+            double linhastotais = dgvPauta.Rows.Count - 1;
+
+            for (int i = 0; i < linhastotais; i++)
+            {
+                if (Convert.ToDouble(dgvPauta.Rows[i].Cells[15].Value) > 12)
+                {
+                    if (Convert.ToDouble(dgvPauta.Rows[i].Cells[16].Value)>1)
+                    {
+                        reprovadosMediaMaior++;
+                    }
+                }
+            }
+            MessageBox.Show(Convert.ToString(reprovadosMediaMaior));
+            txtRespostasDe1a5.Text = "";
+            txtRespostasDe1a5.Text = Convert.ToString(reprovadosMediaMaior);
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ColonizarGrid();
+            ContarNegativas();
+            CalculaMedia();
+        }
+
+        private void lblQuantasRaparigas_Click(object sender, EventArgs e)
+        {
+            contargenerosF();
+        }
+
+        private void lblRapazesVivemEmMaximinios_Click(object sender, EventArgs e)
+        {
+            rapazesVivemEmMaximinios();
+        } 
+        private void lblMaisRapazesOuRaparigas_Click(object sender, EventArgs e)
+        {
+            maisRaparigasOuRapazes();
+        }
+
+        private void lblOmaisVelho_Click(object sender, EventArgs e)
+        {
+            oMaisVelho();
+        }
+
+        private void lblReprovadosTemMediaSuperiorDoze_Click(object sender, EventArgs e)
+        {
+            
+            reprovadosMediaMaiorQueDoze();
+        }
+    }
+}
